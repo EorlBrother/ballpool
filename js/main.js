7d7bd5b9b18ced9eb46b7a47ad753b721a14f752
Carsten Jørgensen <to.bogstaver@gmail.com>
2016-05-28 13:56:21 +0200

    initial stuff

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var game = new Phaser.Game(window.innerHeight, window.innerHeight, Phaser.CANVAS, 'game', {
+    preload: preload,
+    create: create,
+    render: render,
+    update: update
+  },
+  false, false);
+
+
+function preload() {
+}
+
+function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  game.stage.backgroundColor = '#000000'; //'#f46f0a';
+
+}
+
+
+
+function render() {
+}
+
+
+
+function update() {
+
+}
