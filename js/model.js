80c53762fec7c56ca9290d9faa9d700edab63a52
David Magnus Henriques <eorlbruder@magnus-henriques.de>
2016-05-28 14:17:43 +0200

    added Ball-Model

---
+++ js/model.js
@@ -0,0 +1,15 @@
+function Ball (x, y, color) {
+    this.x = x;
+    this.y = y;
+
+    this.color = color;
+
+}
+
+var balls = [];
+
+function generateBalls() ·{
+  for (var i = 0; i<BALL_AMOUNT; i++) {
+    balls[i] = new Ball(Math.random()*(POOL_WIDTH-2*BALL_RADIUS)+BALL_RADIUS, Math.random()*(POOL_HEIGHT-2*BALL_RADIUS)+BALL_RADIUS, COLORS[Math.floor(Math.random()*COLORS_SIZE)])
+  }
+}
